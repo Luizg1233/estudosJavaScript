744df7b457a4c708ece0f9be5a2d1cc94fad1883
Luiz Gabriel <luizgabriel1233@gmail.com>
2022-05-25 16:29:52 -0300

    Primeiras aulas

---
+++ 3_paraticularidades_js/js/script.js
@@ -0,0 +1,21 @@
+// js funciona de cima para baixo
+console.log("Primeiro");
+
+console.log("Último");
+
+// Tipagem fraca, variáveis podem mudar de valor ao longo do código
+
+var a = 1;
+
+a = 'teste';
+
+console.log(a);
+
+// ; é opcional na maioria dos casos, melhor sempre usar
+
+/* É 
+possível comentar de 
+dois jeitos 
+diferentes 
+*/
+
---
+++ 4_variaveis/js/script.js
@@ -0,0 +1,32 @@
+var teste = 1;
+
+var nome = "João";
+
+console.log(teste);
+
+teste = 'Matheus';
+
+console.log(teste);
+
+console.log(nome);
+
+// variável sem declaração se torna de escopo global
+
+// $ e _ são os únicos caracteres especiais que posso usar pra declarar
+
+var _nome = 'Luiz';
+var $nome = 'Gabriel';
+
+console.log(_nome);
+console.log($nome);
+
+let testando = 1;
+const ola = 2;
+
+var meuNome;
+
+console.log(meuNome);
+
+meuNome = 'Gabriel';
+
+console.log(meuNome);
